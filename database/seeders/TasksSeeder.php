0e4d1032229804662d40bb650f6cd4aad88981cd
David van Oudheusden <doudheusden@novacollege.nl>
2025-11-12 09:16:11 +0100

    created task seeder

---
+++ database/seeders/TasksSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TasksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tasks = [
+            [
+                'title' => 'Task 1',
+                'description' => 'Description 1',
+                'completed' => false,
+                'due_date' => '2025-11-11',
+            ],
+            [
+                'title' => 'Task 2',
+                'description' => 'Description 2',
+                'completed' => false,
+                'due_date' => '2025-11-12',
+            ],
+            [
+                'title' => 'Task 3',
+                'description' => 'Description 3',
+                'completed' => false,
+                'due_date' => '2025-11-13',
+            ],
+            [
+                'title' => 'Task 4',
+                'description' => 'Description 4',
+                'completed' => false,
+                'due_date' => '2025-11-14',
+            ],
+        ];
+
+        Task::insert($tasks);
+
+    }
+}
