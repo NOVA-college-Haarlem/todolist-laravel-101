29044f3b706bb1d4c30d3ac6a600494a572c6925
David van Oudheusden <doudheusden@novacollege.nl>
2025-11-19 09:04:21 +0100

    added a few routes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ Route::get('/about', [PageController::class, 'about']);
 
 Route::get('/tasks', [TaskController::class, 'index']);
 
-Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+
 
 
 
