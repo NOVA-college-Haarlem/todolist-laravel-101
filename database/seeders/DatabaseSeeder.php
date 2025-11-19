9605c5773165b10f86f399733015e00fb403b83a
David van Oudheusden <doudheusden@novacollege.nl>
2025-11-19 09:03:29 +0100

    added firstname and lastname to table.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'firstname' => 'Admin Firstname',
+            'lastname' => 'Admin Lastname',
+            'email' => 'admin@admin.com',
         ]);
 
 
